57fdde5637188aac898401cc4092c493f6dbd018
Evan Johnson <ev.jonnson@gmail.com>
2025-11-07 13:08:03 -0700

    eloop first draft

--- lib/eloop.lua
+++ lib/eloop.lua
@@ -15,12 +15,25 @@ function eloop.new()
   i.overdub = 0
   i.prev_time = 0
   i.event = {}
-  i.time = {}
+  i.time = {} -- time[i] is time (secs) between i and i+1, or i and end of loop for last event
+  i.time_total = 0
   i.count = 0
   i.step = 0
   i.time_factor = 1
+  i.sync = false
+  i.sync_div = 1 -- beat on which to start/stop recording
+  i.time_factor_sync_mult = 1
+  i.time_factor_sync_div = 1
 
   i.metro = metro.init(function() i:next_event() end,1,1)
+
+  -- keep track of running clocks
+  i.clocks = {
+    syncer = nil,
+    rec_stop = nil,
+    rec_start = nil,
+    play_start = nil
+  }
 
   i.process = function(_) print("event") end
 
@@ -49,24 +62,45 @@ end
 
 --- start recording
 function eloop:rec_start()
-  print("pattern rec start")
-  self.rec = 1
+  if not self.sync then
+    self.rec = 1
+  elseif not self.clocks.rec_start then
+    self.clocks.rec_start = clock.run(function()
+      clock.sync(self.sync_div)
+      self.rec = 1
+      self.clocks.rec_start = nil
+    end)
+  end
 end
 
 --- stop recording
 function eloop:rec_stop()
   if self.rec == 1 then
-    self.rec = 0
-    if self.count ~= 0 then
-      --print("count "..self.count)
-      local t = self.prev_time
-      self.prev_time = util.time()
-      self.time[self.count] = self.prev_time - t
-      --tab.print(self.time)
-    else
-      print("pattern_time: no events recorded")
-    end 
+    if not self.sync then
+      self:_rec_stop()
+    elseif not self.clocks.rec_stop then
+      self.clocks.rec_stop = clock.run(function()
+        clock.sync(self.sync_div)
+        self:_rec_stop()
+        self.clocks.rec_stop = nil
+      end)
+    end
   else print("pattern_time: not recording")
+  end
+end
+
+function eloop:_rec_stop()
+  self.rec = 0
+  if self.count ~= 0 then
+    --print("count "..self.count)
+    local t = self.prev_time
+    self.prev_time = util.time()
+    local elapsed = self.prev_time - t
+    self.time[self.count] = elapsed
+    self.time_total = self.time_total + elapsed
+    --tab.print(self.time)
+  else
+    print("pattern_time: no events recorded")
   end
 end
 
@@ -87,7 +121,9 @@ function eloop:rec_event(e)
   else
     local t = self.prev_time
     self.prev_time = util.time()
-    self.time[c-1] = self.prev_time - t
+    local elapsed = self.prev_time - t
+    self.time[c-1] = elapsed
+    self.time_total = self.time_total + elapsed
   end
   self.count = c
   self.event[c] = e
@@ -106,38 +142,52 @@ function eloop:overdub_event(e)
   self.count = self.count + 1
 end
 
---- start this pattern
+--- start the loop
 function eloop:start()
   if self.count > 0 then
-    --print("start pattern ")
-    self.prev_time = util.time()
-    self.process(self.event[1])
-    self.play = 1
-    self.step = 1
-    self.metro.time = self.time[1] * self.time_factor
-    self.metro:start()
+    if not self.sync then
+      self.prev_time = util.time()
+      self.process(self.event[1])
+      self.play = 1
+      self.step = 1
+      self.metro.time = self.time[1] * self.time_factor
+      self.metro:start()
+    else
+      self.clocks.syncer = clock.run(self:make_syncer())
+    end
   end
 end
 
 --- process next event
 function eloop:next_event()
   self.prev_time = util.time()
-  if self.step == self.count then self.step = 1
-  else self.step = self.step + 1 end
-  --print("next step "..self.step)
-  --event_exec(self.event[self.step])
+  if self.step == self.count then
+    self.step = 1
+  else
+    self.step = self.step + 1
+  end
   self.process(self.event[self.step])
-  self.metro.time = self.time[self.step] * self.time_factor
-  --print("next time "..self.metro.time)
+  local tf
+  if not self.sync then
+    tf = self.time_factor
+  else
+    tf = self.time_factor_sync_mult / self.time_factor_sync_div
+  end
+  self.metro.time = self.time[self.step] * tf
   self.metro:start()
 end
 
 --- stop this pattern
 function eloop:stop()
   if self.play == 1 then
-    self.play = 0
-    self.overdub = 0
-    self.metro:stop()
+    if not self.sync then
+      self.play = 0
+      self.overdub = 0
+      self.metro:stop()
+    else
+      -- let pattern finish naturally
+      clock.cancel(self.clocks.syncer)
+    end
   else print("pattern_time: not playing") end
 end
 
@@ -147,6 +197,27 @@ function eloop:set_overdub(s)
     self.overdub = 1
   else
     self.overdub = 0
+  end
+end
+
+-- need to capture reference to self in closure to access self from clock
+function eloop:make_syncer()
+  return function()
+    -- on the beat div
+    clock.sync(self.time_total / clock.get_beat_sec())
+
+    -- stop
+    self.play = 0
+    self.overdub = 0
+    self.metro:stop()
+
+    -- then restart
+    self.prev_time = util.time()
+    self.process(self.event[1])
+    self.play = 1
+    self.step = 1
+    self.metro.time = self.time[1] * self.time_factor
+    self.metro:start()
   end
 end
 
